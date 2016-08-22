e5693941a3b84fc2f117a6d8fa32189a1b6af991
Koreil <xenia-kor@ya.ru>
2016-08-22 23:45:30 +0300

    Initial commit

--- tasks/task_6/moment.js
+++ tasks/task_6/moment.js
@@ -2,19 +2,164 @@
 
 module.exports = function () {
     return {
-        // Здесь как-то хранится дата ;)
-        date: null,
-
-        // А здесь часовой пояс
+        week: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
+        daysLeft: ['ПН', 'ВТ', 'СР'],
+        formattedDate: null,
         timezone: null,
+        set date(value) {
+            if (typeof value === 'string') {
+                this.formattedDate = this.recoverDate(value);
+            } else {
+                this.formattedDate = value;
+            }
+        },
+
+        get date() {
+            if (this.timezone) {
+                var UTCHours = new Date(this.formattedDate).getUTCHours();
+                return new Date(this.formattedDate).setUTCHours(UTCHours - this.timezone);
+            }
+            return this.formattedDate;
+        },
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var date = new Date(this.date);
+            var day = this.week[new Date(this.date).getUTCDay()];
+            var hours = date.getUTCHours();
+            var minutes = date.getUTCMinutes();
+
+            hours = (hours > 9) ? hours : '0' + hours;
+            minutes = (minutes > 9) ? minutes : '0' + minutes;
+
+            pattern = pattern.replace('%DD', day);
+            pattern = pattern.replace('%HH:%MM', hours + ':' + minutes);
+
+            return pattern;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var timeRemains = this.date - moment.date;
+            timeRemains = Math.round(timeRemains / 60000);
+            var minutes = Math.floor(timeRemains % 60);
+            timeRemains = timeRemains / 60;
+            var hours = Math.floor(timeRemains % 24);
+            timeRemains = timeRemains - hours;
+            var days = Math.floor(timeRemains / 24);
+
+            days = this.returnDaysCollocation(days);
+            hours = this.returnHoursCollocation(hours);
+            minutes = this.returnMinutesCollocation(minutes);
+
+            var alarm = 'До ограбления ' + days + hours + minutes;
+            return alarm;
+        },
+
+        recoverDate: function (dateString) {
+            // FIX: week постоянно объявляется
+            var week = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+            var datePartsRegExp = /^([А-Я]{0,2})\s?(\d{2}):(\d{2})([+-]\d{1,2})$/;
+            var dateParts = datePartsRegExp.exec(dateString);
+            var parsedDate = new Date();
+
+            var day = week.indexOf(dateParts[1]);
+
+            // получаем ближайший соответвующий день (например, ближайший понедельник)
+            if (parsedDate.getDay()) {
+                day = parsedDate.getDate() + 7 - parsedDate.getDay() + day;
+            } else {
+                day = parsedDate.getDate() + day;
+            }
+
+            var timezone = dateParts[4];
+            var hours = parseInt(dateParts[2]) - timezone;
+            var minutes = parseInt(dateParts[3]);
+
+
+            parsedDate.setUTCDate(day);
+            parsedDate.setUTCHours(hours, minutes, 0);
+
+            return parsedDate.getTime();
+        },
+
+        reviver: function () {
+            var that = this;
+            return function (key, value) {
+                if (key === 'from' || key === 'to') {
+                    return that.recoverDate(value);
+                }
+                return value;
+            };
+        },
+
+        returnDaysCollocation: function (days) {
+            switch (days.toString().slice(-1)) {
+                case '0':
+                    days = '';
+                    break;
+                case '1':
+                    days = days + ' день ';
+                    break;
+
+                case '2':
+                case '3':
+                case '4':
+                    days = days + ' дня ';
+                    break;
+
+                default:
+                    days = days + ' дней ';
+                    break;
+            }
+            return days;
+        },
+
+        returnHoursCollocation: function (hours) {
+            if (hours < 20 && hours > 10) {
+                return hours + ' часов ';
+            }
+
+            switch (hours.toString().slice(-1)) {
+                case '1':
+                    hours = hours + ' час ';
+                    break;
+
+                case '2':
+                case '3':
+                case '4':
+                    hours = hours + ' часа ';
+                    break;
+
+                default:
+                    hours = hours + ' часов ';
+                    break;
+            }
+            return hours;
+        },
+
+        returnMinutesCollocation: function (minutes) {
+            if (minutes < 20 && minutes > 10) {
+                return minutes + ' минут ';
+            }
+
+            switch (minutes.toString().slice(-1)) {
+                case '1':
+                    minutes = minutes + ' минута ';
+                    break;
+
+                case '2':
+                case '3':
+                case '4':
+                    minutes = minutes + ' минуты ';
+                    break;
+
+                default:
+                    minutes = minutes + ' минут ';
+                    break;
+            }
+            return minutes;
         }
     };
 };
--- tasks/task_6/robbery.js
+++ tasks/task_6/robbery.js
@@ -5,11 +5,48 @@ var moment = require('./moment');
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
-
+    var reviver = appropriateMoment.reviver();
     // 1. Читаем json
     // 2. Находим подходящий ближайший момент начала ограбления
     // 3. И записываем в appropriateMoment
 
+    var busyTimeShedule = JSON.parse(json, reviver);
+    var badGuys = Object.keys(busyTimeShedule);
+    var possibleDays = appropriateMoment.daysLeft;
+    // перевожу интервалы в миллисекунды и сортирую по возрастанию (см. sortBusyIntervalsMs)
+
+    var busyIntervalsMs = sortBusyIntervals(badGuys, busyTimeShedule);
+
+    var bankSheduleByDays = getBankSheduleByDays(possibleDays, workingHours);
+    bankSheduleByDays = JSON.stringify(bankSheduleByDays);
+    bankSheduleByDays = JSON.parse(bankSheduleByDays, reviver);
+
+    var robberyMoment;
+    bankSheduleByDays.forEach(workingDay => {
+        // перебираем все занятые интервалы и потом проверяем время между ними (т.е. свободное)
+        for (var i = 0; i < busyIntervalsMs.length - 1; i++) {
+            // для краткости записываем в переменные интервалы занятости
+            var prevBusyInt = busyIntervalsMs[i];
+            var nextBusyInt = busyIntervalsMs[i + 1];
+            // если между концом первого интервала и началом следующего достаточно времени
+            if (isLongEnough(prevBusyInt.to, nextBusyInt.to, minDuration)) {
+                // тогда это может быть подходящим моментом
+                robberyMoment = {
+                    from: prevBusyInt.to,
+                    to: nextBusyInt.to
+                };
+                // но необходимо, чтобы это было после открытия банка и в его рабочие часы
+                // (я исходила из мысли, что ограбление может начаться и за минуту до закрытия,
+                // ведь двери должны быть открыты, чтобы войти)
+                if (hasCrossing(robberyMoment, workingDay) &&
+                workingDay.from <= robberyMoment.from) {
+                    return robberyMoment;
+                }
+            }
+        }
+    });
+
+    appropriateMoment.date = robberyMoment.from;
     return appropriateMoment;
 };
 
@@ -22,3 +59,39 @@ module.exports.getStatus = function (moment, robberyMoment) {
 
     return 'Ограбление уже идёт!';
 };
+
+function isLongEnough(begin, end, duration) {
+    return Boolean(end - begin >= duration);
+}
+
+// Собирает интервалы воедино, переводит в ms и сортирует
+function sortBusyIntervals(badGuys, shedule) {
+    return badGuys
+            .reduce((intervals, guy) => intervals.concat(shedule[guy]), [])
+            .sort((prevInt, nextInt) => prevInt.from - nextInt.from);
+}
+
+// Проверяет интервалы на пересечения
+function hasCrossing(int0, int1) {
+    if (int0.from > int1.to) {
+        return false;
+    }
+    if (int0.to < int1.from) {
+        return false;
+    }
+
+    return true;
+}
+
+// Возвращает для каждого дня интервал, когда банк открыт
+function getBankSheduleByDays(days, workingHours) {
+    return days.map(day => {
+        return {
+            from: day + ' ' + workingHours.from,
+            to: day + ' ' + workingHours.to
+        };
+    });
+}
+
+
+
