1a34fe7f8b339886a814e96e5679420a4d474668
Koreil <xenia-kor@ya.ru>
2016-08-24 09:32:54 +0300

    Learning to resolve conflicts

--- tasks/task_6/moment.js
+++ tasks/task_6/moment.js
@@ -1,31 +1,56 @@
 'use strict';
-
+/**
+* Возвращает объект с методами для работы с датой в заданном формате
+* @returns {Object}
+*/
 module.exports = function () {
     return {
         week: ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'],
         daysLeft: ['ПН', 'ВТ', 'СР'],
-        formattedDate: null,
+        // здесь хранится UTC-дата
+        UTCDateMs: null,
         timezone: null,
+        /**
+         * Сеттер свойства date
+         * предполагается, что дата приходит либо в виде строки вида 'ПН 09:01+5'
+         * либо в виде количества миллисекунд (число)
+         * @param {String} || {Number} value - дата в виде строки или числа
+        */
         set date(value) {
             if (typeof value === 'string') {
-                this.formattedDate = this.recoverDate(value);
+                this.UTCDateMs = this.dateParser(value);
             } else {
-                this.formattedDate = value;
+                this.UTCDateMs = value;
             }
         },
-
         get date() {
             if (this.timezone) {
-                var UTCHours = new Date(this.formattedDate).getUTCHours();
-                return new Date(this.formattedDate).setUTCHours(UTCHours - this.timezone);
+                var UTCHours = new Date(this.UTCDateMs).getUTCHours();
+                return new Date(this.UTCDateMs).setUTCHours(UTCHours - this.timezone);
             }
-            return this.formattedDate;
+            return this.UTCDateMs;
         },
-
-        // Выводит дату в переданном формате
+        /**
+         * Сеттер свойства date
+         * если задан часовой пояс, возвращает дату в этом часовом поясе
+         * при этом UTCDateMs остается в UTC
+         * @returns {Number} - дата в числа в ms
+        */
+        get date() {
+            if (this.timezone) {
+                var currentUTChours = new Date(this.UTCDateMs).getUTCHours();
+                return new Date(this.UTCDateMs).setUTCHours(currentUTChours - this.timezone);
+            }
+            return this.UTCDateMs;
+        },
+        /**
+         * Выводит дату в переданном формате
+         * @param {String} pattern - строка, которую необходимо вернуть с нужными даннными
+         * @returns {String} - строка с нужными данными
+        */
         format: function (pattern) {
             var date = new Date(this.date);
-            var day = this.week[new Date(this.date).getUTCDay()];
+            var day = this.week[date.getUTCDay()];
             var hours = date.getUTCHours();
             var minutes = date.getUTCMinutes();
 
@@ -37,11 +62,14 @@ module.exports = function () {
 
             return pattern;
         },
-
-        // Возвращает кол-во времени между текущей датой и переданной `moment`
-        // в человекопонятном виде
+        /**
+         * Возвращает кол-во времени между текущей датой и переданной `moment`
+         * в человекопонятном виде
+         * @param {Object} moment - объект, хранящий дату
+         * @returns {String} - строка, содержит оставшееся до ограбления время
+        */
         fromMoment: function (moment) {
-            var timeRemains = this.date - moment.date;
+            var timeRemains = this.UTCDateMs - moment.date;
             timeRemains = Math.round(timeRemains / 60000);
             var minutes = Math.floor(timeRemains % 60);
             timeRemains = timeRemains / 60;
@@ -56,66 +84,67 @@ module.exports = function () {
             var alarm = 'До ограбления ' + days + hours + minutes;
             return alarm;
         },
+        /**
+         * Возвращает функцию, которая преобразует строку вида 'ПН 12:59+5' в дату в ms
+         * @returns {Function}
+        */
+        dateParser: function () {
+            var week = this.week;
+            /**
+            * Преобразует строку вида 'ПН 12:59+5' и возвращает дату в ms
+            * @param {String} dateString - дата в виде строки
+            * @returns {Number} - дата в ms
+            */
+            return function (dateString) {
+                var datePartsRegExp = /^([А-Я]{0,2})\s?(\d{2}):(\d{2})([+-]\d{1,2})$/;
+                var dateParts = datePartsRegExp.exec(dateString);
+                var parsedDate = new Date();
 
-        recoverDate: function (dateString) {
-            // FIX: week постоянно объявляется
-            var week = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-            var datePartsRegExp = /^([А-Я]{0,2})\s?(\d{2}):(\d{2})([+-]\d{1,2})$/;
-            var dateParts = datePartsRegExp.exec(dateString);
-            var parsedDate = new Date();
+                var day = week.indexOf(dateParts[1]);
 
-            var day = week.indexOf(dateParts[1]);
+                // получаем ближайший соответвующий день (например, ближайший понедельник)
+                if (parsedDate.getDay()) {
+                    day = parsedDate.getDate() + 7 - parsedDate.getDay() + day;
+                } else {
+                    day = parsedDate.getDate() + day;
+                }
 
-            // получаем ближайший соответвующий день (например, ближайший понедельник)
-            if (parsedDate.getDay()) {
-                day = parsedDate.getDate() + 7 - parsedDate.getDay() + day;
-            } else {
-                day = parsedDate.getDate() + day;
-            }
-
-            var timezone = dateParts[4];
-            var hours = parseInt(dateParts[2]) - timezone;
-            var minutes = parseInt(dateParts[3]);
+                var timezone = dateParts[4];
+                var hours = parseInt(dateParts[2]) - timezone;
+                var minutes = parseInt(dateParts[3]);
 
 
-            parsedDate.setUTCDate(day);
-            parsedDate.setUTCHours(hours, minutes, 0);
+                parsedDate.setUTCDate(day);
+                parsedDate.setUTCHours(hours, minutes, 0);
 
-            return parsedDate.getTime();
+                return parsedDate.getTime();
+            };
         },
-
+        /**
+        * Возвращает функцию, восстанавливающую дату из JSON
+        * @returns {Function}
+        */
         reviver: function () {
-            var that = this;
+            var dateParser = this.dateParser();
+            /**
+            * Применяет функцию dateParser к строкам, обозначающим начало и конец интервала
+            * @param {String} key
+            * @param {String} value
+            * @returns {Number} - дата в ms
+            */
             return function (key, value) {
                 if (key === 'from' || key === 'to') {
-                    return that.recoverDate(value);
+                    return dateParser(value);
                 }
                 return value;
             };
         },
-
-        returnDaysCollocation: function (days) {
-            switch (days.toString().slice(-1)) {
-                case '0':
-                    days = '';
-                    break;
-                case '1':
-                    days = days + ' день ';
-                    break;
-
-                case '2':
-                case '3':
-                case '4':
-                    days = days + ' дня ';
-                    break;
-
-                default:
-                    days = days + ' дней ';
-                    break;
-            }
-            return days;
-        },
-
+        /**
+        * Создает грамматически верное (надеюсь)) словосочетание
+        * в зависимости от количества часов
+        * @param {Number} hours
+        * @returns {String} - словосочетание
+        */
         returnHoursCollocation: function (hours) {
             if (hours < 20 && hours > 10) {
                 return hours + ' часов ';
@@ -135,10 +164,42 @@ module.exports = function () {
                 default:
                     hours = hours + ' часов ';
                     break;
-            }
+            };
             return hours;
         },
+        /**
+        * Создает грамматически верное словосочетание
+        * в зависимости от количества дней
+        * @param {Number} days
+        * @returns {String} - словосочетание
+        */
+        returnDaysCollocation: function (days) {
+            switch (days.toString().slice(-1)) {
+                case '0':
+                    days = '';
+                    break;
+                case '1':
+                    days = days + ' день ';
+                    break;
 
+                case '2':
+                case '3':
+                case '4':
+                    days = days + ' дня ';
+                    break;
+
+                default:
+                    days = days + ' дней ';
+                    break;
+            };
+            return days;
+        },
+        /**
+        * Создает грамматически верное словосочетание
+        * в зависимости от количества минут
+        * @param {Number} minutes
+        * @returns {String} - словосочетание
+        */
         returnMinutesCollocation: function (minutes) {
             if (minutes < 20 && minutes > 10) {
                 return minutes + ' минут ';
